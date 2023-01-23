f552971f66c848f4c832dffceffbef8ca4f368a1
Hongsen Yang <hongsen.m.yang@gmail.com>
2023-01-22 19:15:28 -0800

    Updated tab spacing.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,105 +5,105 @@
  */
 
 module.exports = {
-  // Enables server side render (SSR) in develop - used to detect SSR bugs
-  flags: {
-    DEV_SSR: true
-  },
+	// Enables server side render (SSR) in develop - used to detect SSR bugs
+	flags: {
+		DEV_SSR: true
+	},
 
-  siteMetadata: {
-    title: 'Hongsen Yang',
-    description: 'Portfolio website of Hongsen Yang. Photographer, designer, and RF engineer based in San Deigo, CA.',
-    author: {
-      name: 'Hongsen Yang',
-    },
-    keywords: ['photography', 'travel', 'portrait', 'design', 'illustration', 'portfolio'],
-    siteUrl: 'https://www.hongsenyang.com',
-  },
+	siteMetadata: {
+		title: 'Hongsen Yang',
+		description: 'Portfolio website of Hongsen Yang. Photographer, designer, and RF engineer based in San Deigo, CA.',
+		author: {
+			name: 'Hongsen Yang',
+		},
+		keywords: ['photography', 'travel', 'portrait', 'design', 'illustration', 'portfolio'],
+		siteUrl: 'https://www.hongsenyang.com',
+	},
 
-  plugins: [
-    // Generates favicon for browser tabs
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Hongsen Yang`,
-        short_name: `Hongsen`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#121212`,
-        display: `minimal-ui`,
-        icon: `content/assets/favicon.png`,
-      },
-    },
+	plugins: [
+		// Generates favicon for browser tabs
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Hongsen Yang`,
+				short_name: `Hongsen`,
+				start_url: `/`,
+				background_color: `#ffffff`,
+				theme_color: `#121212`,
+				display: `minimal-ui`,
+				icon: `content/assets/favicon.png`,
+			},
+		},
 
-    // CSS Plugins
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        // Override the file regex for CSS modules (see https://www.gatsbyjs.com/plugins/gatsby-plugin-sass/)
-        // sassRuleModulesTest: /style\.s(a|c)ss$/,
-      },
-    },
+		// CSS Plugins
+		{
+			resolve: `gatsby-plugin-sass`,
+			options: {
+				// Override the file regex for CSS modules (see https://www.gatsbyjs.com/plugins/gatsby-plugin-sass/)
+				// sassRuleModulesTest: /style\.s(a|c)ss$/,
+			},
+		},
 
-    // Image plugins
-    `gatsby-plugin-image`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        useMozJpeg: false,
-        stripMetadata: true,
-        defaultQuality: 100,
-      }
-    },
-    {
-      resolve: `gatsby-plugin-react-svg`,
-      options: {
-        rule: {
-          include: /assets/
-        }
-      }
-    },
+		// Image plugins
+		`gatsby-plugin-image`,
+		`gatsby-transformer-sharp`,
+		{
+			resolve: `gatsby-plugin-sharp`,
+			options: {
+				useMozJpeg: false,
+				stripMetadata: false,
+				defaultQuality: 100,
+			}
+		},
+		{
+			resolve: `gatsby-plugin-react-svg`,
+			options: {
+				rule: {
+					include: /assets/
+				}
+			}
+		},
 
-    // File parsers
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-transformer-yaml`,
-      options: {
-        typeName: `Yaml`,
-      }
-    },
+		// File parsers
+		`gatsby-transformer-remark`,
+		{
+			resolve: `gatsby-transformer-yaml`,
+			options: {
+				typeName: `Yaml`,
+			}
+		},
 
-    // gatsby-source-filesystem
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      }
-    }, 
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/pages`,
-        name: `pages`,
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/images`,
-        name: `images`,
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/galleries`,
-        name: `galleries`,
-      }
-    },
+		// gatsby-source-filesystem
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/assets`,
+				name: `assets`,
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/pages`,
+				name: `pages`,
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/images`,
+				name: `images`,
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/galleries`,
+				name: `galleries`,
+			}
+		},
 
-    // Other
-    `gatsby-plugin-react-helmet`,
-  ],
+		// Other
+		`gatsby-plugin-react-helmet`,
+	],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,60 +2,60 @@ const path = require(`path`);
 // const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
+    const { createPage } = actions;
 
-  const result = await graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              frontmatter {
-                slug
-                type
-              }
+    const result = await graphql(
+        `
+            {
+                allMarkdownRemark(
+                    sort: { fields: [frontmatter___date], order: DESC }
+                    limit: 1000
+                ) {
+                    edges {
+                        node {
+                            frontmatter {
+                                slug
+                                type
+                            }
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
-    `
-  );
+        `
+    );
 
-  if (result.errors) {
-    throw result.errors;
-  }
-
-  // Create blog posts pages.
-  const pages = result.data.allMarkdownRemark.edges;
-
-  const galleryPage = path.resolve(`./src/templates/gallery-template.js`);
-  const designPage = path.resolve(`./src/templates/design-template.js`);
-  pages.forEach((page, index) => {
-    // const previous = index === pages.length - 1 ? null : pages[index + 1].node;
-    // const next = index === 0 ? null : pages[index - 1].node;
-
-    const componentPage = () => {
-      switch (page.node.frontmatter.type) {
-        case 'design':
-          return designPage;
-        default:
-          return galleryPage;
-      }
+    if (result.errors) {
+        throw result.errors;
     }
 
-    // The slug is used as a Graphql variable in the template's graphql query.
-    // See https://www.gatsbyjs.com/docs/creating-and-modifying-pages/#creating-pages-in-gatsby-nodejs.
-    createPage({
-      path: page.node.frontmatter.slug,
-      component: componentPage(),
-      context: {
-        slug: page.node.frontmatter.slug,
-        // previous,
-        // next,
-      },
+    // Create blog posts pages.
+    const pages = result.data.allMarkdownRemark.edges;
+
+    const galleryPage = path.resolve(`./src/templates/gallery-template.js`);
+    const designPage = path.resolve(`./src/templates/design-template.js`);
+    pages.forEach((page, index) => {
+        // const previous = index === pages.length - 1 ? null : pages[index + 1].node;
+        // const next = index === 0 ? null : pages[index - 1].node;
+
+        const componentPage = () => {
+            switch (page.node.frontmatter.type) {
+                case 'design':
+                    return designPage;
+                default:
+                    return galleryPage;
+            }
+        }
+
+        // The slug is used as a Graphql variable in the template's graphql query.
+        // See https://www.gatsbyjs.com/docs/creating-and-modifying-pages/#creating-pages-in-gatsby-nodejs.
+        createPage({
+            path: page.node.frontmatter.slug,
+            component: componentPage(),
+            context: {
+                slug: page.node.frontmatter.slug,
+                // previous,
+                // next,
+            },
+        });
     });
-  });
 };
